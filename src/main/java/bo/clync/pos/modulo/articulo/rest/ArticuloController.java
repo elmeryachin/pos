a9236e4b7cbf704dcc6f23be6efec5d96ca2e8d2
elmeryachin <elmeryachin@gmail.com>
2017-10-11 17:21:07 -0400

    Se quita el token header

--- src/main/java/bo/clync/pos/modulo/articulo/rest/ArticuloController.java
+++ src/main/java/bo/clync/pos/modulo/articulo/rest/ArticuloController.java
@@ -20,36 +20,32 @@ public class ArticuloController {
 
     @Autowired
     private Servicio service;
-
+    private String token = "default";
     @RequestMapping(value = "/articulo", method = RequestMethod.GET)
-    public ResponseEntity<ServListaResponse> lista(@RequestHeader("token") String token) {
+    public ResponseEntity<ServListaResponse> lista() {//@RequestHeader("token") String token
         return new ResponseEntity<>(service.lista(token), HttpStatus.OK);
     }
 
     //recuperar el objeto
     @RequestMapping(value = "/articulo/{codigo}", method = RequestMethod.GET)
-    public ResponseEntity<ServObtenerResponse> obtener(@PathVariable("codigo") String codigo,
-                                                       @RequestHeader("token") String token) {
+    public ResponseEntity<ServObtenerResponse> obtener(@PathVariable("codigo") String codigo) {
         return new ResponseEntity<>(service.obtener(codigo, token), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/articulo/", method = RequestMethod.POST)
-    public ResponseEntity<ServResponse> nuevo(@RequestHeader("token") String token,
-                                              @RequestBody ServRequest request) {
+    public ResponseEntity<ServResponse> nuevo(@RequestBody ServRequest request) {
         return new ResponseEntity<>(service.nuevo(request, token), HttpStatus.CREATED);
     }
 
     @RequestMapping(value = "/articulo/{codigo}", method = RequestMethod.PUT)
-    public ResponseEntity<ServResponse> update(@RequestHeader("token") String token,
-                                               @PathVariable("codigo") String codigo,
+    public ResponseEntity<ServResponse> update(@PathVariable("codigo") String codigo,
                                                @RequestBody ServRequest request) {
         System.out.println("Ingresando a actualizar. " + request);
         return new ResponseEntity<>(service.actualizar(codigo, request, token), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/articulo/{codigo}", method = RequestMethod.DELETE)
-    public ResponseEntity<ServResponse> delete(@RequestHeader("token") String token,
-                                    @PathVariable("codigo") String codigo) {
+    public ResponseEntity<ServResponse> delete(@PathVariable("codigo") String codigo) {
         return new ResponseEntity<>(service.baja(codigo, token), HttpStatus.OK);
     }
 
