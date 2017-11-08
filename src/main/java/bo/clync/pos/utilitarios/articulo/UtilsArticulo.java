40313e47c65ad4dc0717df2d551e900b2eb65369
elmeryachin <elmeryachin@gmail.com>
2017-11-08 10:42:47 -0400

    mas errores

--- src/main/java/bo/clync/pos/utilitarios/articulo/UtilsArticulo.java
+++ src/main/java/bo/clync/pos/utilitarios/articulo/UtilsArticulo.java
@@ -1,10 +1,5 @@
 package bo.clync.pos.utilitarios.articulo;
 
-import bo.clync.pos.entity.Articulo;
-import bo.clync.pos.model.articulo.obtener.ObjetoArticulo;
-
-import java.util.Date;
-
 /**
  * Created by eyave on 09-10-17.
  */
