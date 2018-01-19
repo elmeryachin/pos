16365b8c9dd1f8a3c3942552738cd4ce66280244
elmeryachin <elmeryachin@gmail.com>
2018-01-19 09:15:47 -0400

    completando

--- src/main/java/bo/clync/pos/dao/ServPatron.java
+++ src/main/java/bo/clync/pos/dao/ServPatron.java
@@ -4,4 +4,14 @@ package bo.clync.pos.dao;
  * Created by eyave on 19-01-18.
  */
 public class ServPatron {
+    private String patron;
+
+
+    public String getPatron() {
+        return patron;
+    }
+
+    public void setPatron(String patron) {
+        this.patron = patron;
+    }
 }
