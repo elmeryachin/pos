e36ebd2a744f782ab737ddd9f550c6e0c0eb4ccf
elmeryachin <elmeryachin@gmail.com>
2018-03-10 08:48:54 -0400

    creando war

--- src/main/java/bo/clync/pos/App.java
+++ src/main/java/bo/clync/pos/App.java
@@ -1,14 +1,28 @@
 package bo.clync.pos;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Created by eyave on 05-10-17.
  */
 @SpringBootApplication
-public class App {
+public class App extends SpringBootServletInitializer {
+
+
     public static void main( String[] args ) {
         SpringApplication.run(App.class);
     }
+/*
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(App.class);
+    }
+
+    private static SpringApplicationBuilder configureApplication(SpringApplicationBuilder builder) {
+        return builder.sources(App.class).bannerMode(Banner.Mode.OFF);
+    }*/
 }
