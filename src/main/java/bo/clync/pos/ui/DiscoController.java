6f54c732efa101dc2779c30f05c35c46d4c05e81
elmeryachin <elmeryachin@gmail.com>
2019-02-22 11:07:41 -0400

    Disco controller modificado para funcion de listar, grabar y actualizar

--- src/main/java/bo/clync/pos/ui/DiscoController.java
+++ src/main/java/bo/clync/pos/ui/DiscoController.java
@@ -55,87 +55,89 @@ public class DiscoController {
     @RequestMapping(value="/grabando", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<?> grabando(@RequestHeader(value="token") String token,
                                       @RequestParam("file") MultipartFile file) throws Exception {
+        try {
+            String nombreCompleto = file.getOriginalFilename();
+            String nombre = file.getName();
 
-        String nombreCompleto = file.getOriginalFilename();
-        String nombre = file.getName();
-
-        if(service.verificarProcesoExterno(nombre)) {
-            throw new Exception("El proceso ya existe");
-        }
-
-        File uploadedFile = new File(tmpdir, nombreCompleto);
-        uploadedFile.createNewFile();
-        FileOutputStream fileOutputStream = new FileOutputStream(uploadedFile);
-        fileOutputStream.write(file.getBytes());
-
-        ZipFile zipFile = new ZipFile(uploadedFile);
-
-        zipFile.setPassword("elmeryachin");
-
-        List list = zipFile.getFileHeaders();
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < list.size(); i++) {
-            FileHeader f = (FileHeader) list.get(i);
-            String fileName = f.getFileName();
-            if(fileName.contains(".json")) {
-                File destino = new File("destino.json");
-                ZipInputStream is = zipFile.getInputStream(f);
-
-                OutputStream out = new FileOutputStream(destino);
-
-                int readLen = -1;
-                while ((readLen = is.read()) != -1)
-                {
-                    out.write(readLen);
-                }
-
-                is.close();
-                out.close();
-
-                FileReader fr = new FileReader(destino);
-                BufferedReader br = new BufferedReader(fr);
-                String sCurrentLine;
-                while ((sCurrentLine = br.readLine()) != null) {
-                    sb.append(sCurrentLine);
-                }
-                br.close();
-                fr.close();
-                destino.deleteOnExit();
+            if (service.verificarProcesoExterno(nombre)) {
+                throw new Exception("El proceso ya existe");
             }
-        }
 
-        ObjectMapper mapper = new ObjectMapper();
-        ArrayList<AbcOperaciones> lst = mapper.readValue(sb.toString(),
-                mapper.getTypeFactory().constructCollectionType(ArrayList.class, AbcOperaciones.class));
+            File uploadedFile = new File(tmpdir, nombreCompleto);
+            uploadedFile.createNewFile();
+            FileOutputStream fileOutputStream = new FileOutputStream(uploadedFile);
+            fileOutputStream.write(file.getBytes());
 
-        String http =  "http://localhost:8080";
-        String tokenProceso = null;
-        Date fecha = null;
-        for (int i = 0; i < lst.size(); i++) {
-            AbcOperaciones operaciones = lst.get(i);
-            tokenProceso = operaciones.getToken();
-            fecha = operaciones.getFecha();
-            String url = http + operaciones.getUrl();
-            if(operaciones.getTipo().equals("POST")) {
-                getEnviarPeticionPost(url, operaciones.getJson(), operaciones.getToken());
-            } else if (operaciones.getTipo().equals("PUT")){
-                getEnviarPeticionPut(url, operaciones.getJson(), operaciones.getToken());
-            } else if (operaciones.getTipo().equals("DELETE")){
-                getEnviarPeticionDelete(url, operaciones.getToken());
+            ZipFile zipFile = new ZipFile(uploadedFile);
+
+            zipFile.setPassword("elmeryachin");
+
+            List list = zipFile.getFileHeaders();
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < list.size(); i++) {
+                FileHeader f = (FileHeader) list.get(i);
+                String fileName = f.getFileName();
+                if (fileName.contains(".json")) {
+                    File destino = new File("destino.json");
+                    ZipInputStream is = zipFile.getInputStream(f);
+
+                    OutputStream out = new FileOutputStream(destino);
+
+                    int readLen = -1;
+                    while ((readLen = is.read()) != -1) {
+                        out.write(readLen);
+                    }
+
+                    is.close();
+                    out.close();
+
+                    FileReader fr = new FileReader(destino);
+                    BufferedReader br = new BufferedReader(fr);
+                    String sCurrentLine;
+                    while ((sCurrentLine = br.readLine()) != null) {
+                        sb.append(sCurrentLine);
+                    }
+                    br.close();
+                    fr.close();
+                    destino.deleteOnExit();
+                }
             }
-            System.out.println("c $$$$$$$$$$$$$$$$$$  " + i);
-            System.out.println("" + operaciones.getId());
-            System.out.println(operaciones.getProceso());
-            System.out.println(operaciones.getJson());
-            System.out.println(operaciones.getTipo());
-            System.out.println(operaciones.getToken());
-            System.out.println(operaciones.getUrl());
+
+            ObjectMapper mapper = new ObjectMapper();
+            ArrayList<AbcOperaciones> lst = mapper.readValue(sb.toString(),
+                    mapper.getTypeFactory().constructCollectionType(ArrayList.class, AbcOperaciones.class));
+
+            String http = "http://localhost:8080";
+            String tokenProceso = null;
+            Date fecha = null;
+            for (int i = 0; i < lst.size(); i++) {
+                AbcOperaciones operaciones = lst.get(i);
+                tokenProceso = operaciones.getToken();
+                fecha = operaciones.getFecha();
+                String url = http + operaciones.getUrl();
+                if (operaciones.getTipo().equals("POST")) {
+                    getEnviarPeticionPost(url, operaciones.getJson(), operaciones.getToken());
+                } else if (operaciones.getTipo().equals("PUT")) {
+                    getEnviarPeticionPut(url, operaciones.getJson(), operaciones.getToken());
+                } else if (operaciones.getTipo().equals("DELETE")) {
+                    getEnviarPeticionDelete(url, operaciones.getToken());
+                }
+                System.out.println("c $$$$$$$$$$$$$$$$$$  " + i);
+                System.out.println("" + operaciones.getId());
+                System.out.println(operaciones.getProceso());
+                System.out.println(operaciones.getJson());
+                System.out.println(operaciones.getTipo());
+                System.out.println(operaciones.getToken());
+                System.out.println(operaciones.getUrl());
+            }
+
+            //Se guardar el id proceso en los registros segun el token
+            service.getGrabarCodigoProceso(tokenProceso, nombre, fecha);
+
+            return new ResponseEntity<>("Se actualizo exitosamente la base de datos", HttpStatus.OK);
+        } catch (Exception e ) {
+            return new ResponseEntity<>("Error: " + e.getMessage(), HttpStatus.OK);
         }
-
-        //Se guardar el id proceso en los registros segun el token
-        service.getGrabarCodigoProceso(tokenProceso, nombre, fecha);
-
-        return new ResponseEntity<>( "Se actualizo exitosamente la base de datos", HttpStatus.OK);
     }
 
     @CrossOrigin
@@ -266,138 +268,6 @@ public class DiscoController {
 
         }
         return respuesta;
-    }
-
-    @CrossOrigin
-    @RequestMapping(value = "/lista/procesados", method = RequestMethod.GET)
-    public ResponseEntity<Object> getProcesados(@RequestHeader(value="token") String token ) {
-
-        return null;
-    }
-
-    @CrossOrigin
-    @RequestMapping(value="/upload", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<?> grabar(@RequestHeader(value="token") String token,
-                                    @RequestParam("file") MultipartFile file) throws Exception {
-
-        ObjectMapper mapper = new ObjectMapper();
-        ArrayList<AbcOperaciones> list = mapper.readValue(new File("abcOperaciones.json"),
-                mapper.getTypeFactory().constructCollectionType(ArrayList.class, AbcOperaciones.class));
-
-        for (int i = 0; i < list.size(); i++) {
-            AbcOperaciones o = list.get(i);
-            boolean respuesta = false;
-            if(o.getTipo().equals("")) {
-                respuesta = this.getEnviarPeticionPost(o.getUrl(), o.getJson(), o.getToken());
-            } else if(o.getTipo().equals("")) {
-                respuesta = this.getEnviarPeticionPut(o.getUrl(), o.getJson(), o.getToken());
-            } else if( o.getTipo().equals("")) {
-                respuesta = this.getEnviarPeticionDelete(o.getUrl(), o.getToken());
-            }
-            if(respuesta) {
-                //Actualizar Registro.
-            } else {
-                //throws new Exception("Actualizacion incompleta");
-            }
-            System.out.println("$$$$$$$$$$$$$$$$$$  " + i + respuesta);
-            System.out.println(o.getId());
-            System.out.println(o.getProceso());
-            System.out.println(o.getJson());
-            System.out.println(o.getTipo());
-            System.out.println(o.getToken());
-            System.out.println(o.getUrl());
-        }
-
-        return new ResponseEntity<>( "Subida Exitosa", HttpStatus.OK);
-    }
-
-    @CrossOrigin
-    @RequestMapping(value = "/downloadFile/{fileName:.+}", method = RequestMethod.GET, produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-    public ResponseEntity<Object> downloadFile(@PathVariable String fileName, HttpServletResponse response) {
-
-        // Generar
-        // Firmar
-        // Comprimir + contrasena
-        // Eliminar sueltos
-        // Enviar
-        String filePath = "/home/eyave/Desktop/entrenamiento/athean-x/" + fileName;// + ".txt";
-        Path path = Paths.get(filePath);
-        Resource resource = null;
-
-        try {
-            resource = new UrlResource(path.toUri());
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-
-        if (resource.exists()) {
-            return ResponseEntity.ok()
-                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
-                    .body(resource);
-
-            // return new ResponseEntity<Object>(resource, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<Object>("File Not Found ", HttpStatus.OK);
-        }
-    }
-
-    @CrossOrigin
-    @PostMapping("/leer")
-    public ResponseEntity<?> leer(@RequestHeader(value="token") String token,
-                                  Byte[] array) {
-        return null;// new ResponseEntity<>(service.leer(token, array),HttpStatus.OK);
-    }
-
-    @CrossOrigin
-    @PostMapping("/recuperar")
-    public ResponseEntity<?> recuperar(@RequestHeader(value="token") String token,
-                                       DiscoRequest request) {
-        return null;//new ResponseEntity<>(service.recuperar(token), HttpStatus.OK);
-    }
-
-    @CrossOrigin
-    @RequestMapping(value="/uploadtest", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<?> grabartest(@RequestHeader(value="token") String token,
-                                        @RequestParam("file") MultipartFile file) {
-
-        File uploadedFile = new File("/home/eyave/Desktop/entrenamiento/athean-x/", file.getOriginalFilename());
-        try{
-
-            uploadedFile.createNewFile();
-            FileOutputStream fileOutputStream = new FileOutputStream(uploadedFile);
-            fileOutputStream.write(file.getBytes());
-            //fileOutputStream.close();
-
-            FileReader f = new FileReader(uploadedFile);
-            BufferedReader b = new BufferedReader(f);
-            String cadena = null;
-            StringBuffer sb = new StringBuffer();
-            while((cadena = b.readLine())!=null) {
-                sb.append(cadena);
-            }
-
-            ObjectMapper mapper = new ObjectMapper();
-            List<AbcOperaciones> list = mapper.readValue(sb.toString(), List.class);
-            /*for (int i = 0; i < list.size(); i++) {
-                AbcOperaciones operaciones = list.get(i);
-                System.out.println("###################### " + i);
-                System.out.println(operaciones.getId());
-                System.out.println(operaciones.getProceso());
-                System.out.println(operaciones.getJson());
-                System.out.println(operaciones.getTipo());
-                System.out.println(operaciones.getToken());
-                System.out.println(operaciones.getUrl());
-            }*/
-
-
-            fileOutputStream.close();
-            b.close();
-            //System.out.println("CADENA: " +sb.toString());
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return new ResponseEntity<>( "Subida Exitosa", HttpStatus.OK);
     }
 
 }
